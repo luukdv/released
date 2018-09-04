b7b4e9e86b40655dabeaeb7a1fde7e59a74242e6
luukdv <info@luuk.site>
2018-09-04 17:15:25 +0200

    :wrench: Babel loader

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,20 @@ module.exports = {
     open: true,
   },
   entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        include: path.resolve(__dirname, 'src'),
+        test: /\.js/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
