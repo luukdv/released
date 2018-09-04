611adb2a3032497034ab91046d065e88665ab3a2
luukdv <info@luuk.site>
2018-09-04 16:45:09 +0200

    :wrench: Webpack filename

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   },
   entry: './src/index.js',
   output: {
-    filename: 'dist/bundle.js',
-    path: __dirname,
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
   }
 };
