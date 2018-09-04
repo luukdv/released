e36ae71cb9a7c20bf786d2ac7d908bda1626f3d2
luukdv <info@luuk.site>
2018-09-04 16:43:13 +0200

    :wrench: Setup dev server

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,14 @@
 const path = require('path');
 
 module.exports = {
+  devServer: {
+    compress: true,
+    contentBase: 'dist',
+    open: true,
+  },
   entry: './src/index.js',
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
+    filename: 'dist/bundle.js',
+    path: __dirname,
+  }
 };
