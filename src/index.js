739b24f9e78051975659da028d118f653fea4bf7
luukdv <info@luuk.site>
2018-09-06 10:45:46 +0200

    :construction: Search component

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
+import Search from './Search'
 
 class App extends Component {
   render() {
-    return null;
+    return <Search />;
   }
 }
 
