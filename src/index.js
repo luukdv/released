b42575f33433b916f268481efdade8527e3cb5b0
luukdv <info@luuk.site>
2018-09-06 10:42:54 +0200

    :construction: App component

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+
+class App extends Component {
+  render() {
+    return null;
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector('.app'));
