d2fdcd81799444f0ead5f1ae7c3ca55d7cfdfe2b
luukdv <info@luuk.site>
2018-09-07 16:52:35 +0200

    :wrench: Separate configs for app and Lambda's

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,70 @@
 const fs = require('fs');
 const path = require('path');
-const entry = { './bundle': './src/index.js' };
+const Uglify = require('uglifyjs-webpack-plugin');
 
-fs.readdirSync(path.resolve(__dirname, 'src/functions')).forEach(file => {
-  entry['../functions/' + file.replace('.js', '')] = './src/functions/' + file;
-});
-
-module.exports = {
-  devServer: {
-    compress: true,
-    contentBase: path.resolve(__dirname, 'public'),
-    open: true,
-  },
-  entry,
-  module: {
-    rules: [
-      {
-        include: path.resolve(__dirname, 'src'),
-        test: /\.js/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              '@babel/preset-env',
-              '@babel/preset-react',
-            ],
-          },
+const config = {
+  optimization: {
+    minimizer: [
+      new Uglify({
+        uglifyOptions: {
+          output: { comments: false },
         },
-      },
+      }),
     ],
   },
-  output: {
-    filename: '[name].js',
-    path: path.resolve(__dirname, 'public'),
+};
+const rule = {
+  include: path.resolve(__dirname, 'src'),
+  test: /\.js/,
+  use: {
+    loader: 'babel-loader',
+    options: {
+      presets: [
+        '@babel/preset-env',
+      ],
+    },
   },
 };
+
+module.exports = [
+  (() => {
+    const bundleConfig = Object.assign({}, config);
+    const bundleRule = Object.assign({}, rule);
+
+    bundleRule.use.options.presets.push('@babel/preset-react');
+    bundleConfig.devServer = {
+      compress: true,
+      contentBase: path.resolve(__dirname, 'public'),
+      open: true,
+    };
+    bundleConfig.entry = './src/index.js';
+    bundleConfig.module = { rules: [bundleRule] };
+    bundleConfig.output = {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'public'),
+    };
+
+    return bundleConfig;
+  })(),
+  (() => {
+    const entries = {};
+    const functionsConfig = Object.assign({}, config);
+    const functionsRule = Object.assign({}, rule);
+
+    fs.readdirSync(path.resolve(__dirname, 'src/functions'))
+      .filter(file => file.includes('.js'))
+      .forEach(file => {
+        entries['../functions/' + file.replace('.js', '')] = './src/functions/' + file;
+      });
+
+    functionsConfig.entry = entries;
+    functionsConfig.module = { rules: [functionsRule] };
+    functionsConfig.output = {
+      filename: '[name].js',
+      libraryTarget: 'commonjs',
+      path: path.resolve(__dirname, 'public'),
+    };
+
+    return functionsConfig;
+  })(),
+];
