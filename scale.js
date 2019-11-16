05276865a67fe621321bbdcac69c9f1ecff30be0
Luuk de Vlieger <info@luuk.site>
2019-11-16 19:52:34 +0100

    :sparkles: Scale helper

---
+++ scale.js
@@ -0,0 +1,9 @@
+export default (number, type, strong = false) => `
+  ${type}: calc(${(number / (strong ? 2.25 * 1.5 : 2.25)).toFixed(1)}vw + ${(
+  number / (strong ? 1.5 * 1.5 : 1.5)
+).toFixed(1)}em);
+
+  @media (min-width: 1201px) {
+    ${type}: ${number}em;
+  }
+`
