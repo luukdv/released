8cea931787fa9f4ceef249d209eb43c08500be84
Luuk de Vlieger <info@luuk.site>
2019-11-16 20:46:07 +0100

    :wrench: Gatsby config

---
+++ gatsby-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  plugins: [
+    'gatsby-plugin-emotion',
+    'gatsby-plugin-netlify',
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-remove-trailing-slashes',
+  ],
+}
