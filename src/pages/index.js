9050041e10276772ad5217dd98cb1716943677a3
Luuk de Vlieger <info@luuk.site>
2019-11-16 20:46:19 +0100

    :lipstick: Label tags

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,7 @@
+import scale from '../../scale'
 import Head from '../components/head'
 import Wrap from '../components/wrap'
+import { css } from '@emotion/core'
 import React from 'react'
 import Styles from '../components/styles'
 
@@ -11,10 +13,76 @@ export default () => (
       <div>
         <h2>New releases</h2>
       </div>
-      <div>
+      <div
+        css={css`
+          width: 40%;
+        `}
+      >
         <h2>Add new label</h2>
         <h2>Saved labels</h2>
+        <div
+          css={css`
+            display: flex;
+            flex-wrap: wrap;
+          `}
+        >
+        </div>
       </div>
     </Wrap>
   </>
+)
+
+const Label = ({ name }) => (
+  <div
+    css={css`
+      color: white;
+      font-weight: 700;
+      margin: 0 0.75em 0.75em 0;
+      position: relative;
+      ${scale(1, 'font-size')}
+
+      &:hover .remove {
+        opacity: 1;
+        transform: scale(1);
+        pointer-events: auto;
+      }
+    `}
+  >
+    <div
+      css={css`
+        background: rgb(160, 160, 160);
+        border-radius: 9em;
+        user-select: none;
+        text-align: center;
+        line-height: 1.25;
+        padding: 0.5em 1em;
+      `}
+    >
+      {name}
+    </div>
+    <div
+      className="remove"
+      css={css`
+        background: rgb(255, 40, 80);
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        transform: scale(0.5) translate(-1em, 1em);
+        transition: transform 0.2s ease-out;
+        will-change: transform;
+        width: 2em;
+        height: 2em;
+        pointer-events: none;
+        opacity: 0;
+        border-radius: 50%;
+        position: absolute;
+        cursor: pointer;
+        top: -0.75em;
+        right: -0.75em;
+        z-index: 1;
+      `}
+    >
+      ✕
+    </div>
+  </div>
 )
