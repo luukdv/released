a8b1d52b2687678d259eede7ae4a8894b3274971
Luuk de Vlieger <info@luuk.site>
2019-11-16 19:54:44 +0100

    :sparkles: Wrapper

---
+++ src/components/wrap.js
@@ -0,0 +1,27 @@
+import scale from '../../scale'
+import React from 'react'
+import { css } from '@emotion/core'
+
+export default () => (
+  <div
+    css={css`
+      ${scale(3, 'margin-bottom')}
+    `}
+  >
+    <div
+      css={css`
+        margin: 0 auto;
+        max-width: 1200px;
+        padding-left: 20px;
+        padding-right: 20px;
+
+        @media (min-width: 641px) {
+          padding-left: 40px;
+          padding-right: 40px;
+        }
+      `}
+    >
+      {children}
+    </div>
+  </div>
+)
