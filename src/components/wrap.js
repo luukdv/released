8435cc32e8a2b242d8b9a82f5b51bf13ccfd4c47
Luuk de Vlieger <info@luuk.site>
2019-11-16 20:46:00 +0100

    :lipstick: Wrap

--- src/components/wrap.js
+++ src/components/wrap.js
@@ -1,27 +1,22 @@
-import scale from '../../scale'
 import React from 'react'
 import { css } from '@emotion/core'
 
 export default ({ children }) => (
   <div
     css={css`
-      ${scale(3, 'margin-bottom')}
+      display: flex;
+      justify-content: space-between;
+      margin: 0 auto;
+      max-width: 1200px;
+      padding-left: 20px;
+      padding-right: 20px;
+
+      @media (min-width: 641px) {
+        padding-left: 40px;
+        padding-right: 40px;
+      }
     `}
   >
-    <div
-      css={css`
-        margin: 0 auto;
-        max-width: 1200px;
-        padding-left: 20px;
-        padding-right: 20px;
-
-        @media (min-width: 641px) {
-          padding-left: 40px;
-          padding-right: 40px;
-        }
-      `}
-    >
-      {children}
-    </div>
+    {children}
   </div>
 )
