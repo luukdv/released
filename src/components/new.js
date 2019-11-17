67115e1d2fa4ede04790ffb774d8cfeb842f6868
Luuk de Vlieger <info@luuk.site>
2019-11-17 19:31:53 +0100

    :construction: Date prop for release

--- src/components/new.js
+++ src/components/new.js
@@ -5,29 +5,41 @@ import { css } from '@emotion/core'
 export default () => (
   <>
     <h2>New releases</h2>
-    <Release title="Dusk To Dawn Part I-III" artist="Mr. Tophat" label="" />
-    <Release title="Studio Barnhus Volym 1" artist="Various" label="" />
-    <Release title="Once Upon A Passion" artist="Bella Boo" label="" />
+    <Release
+      title="Dusk To Dawn Part I-III"
+      artist="Mr. Tophat"
+      label=""
+      date=""
+    />
+    <Release title="Studio Barnhus Volym 1" artist="Various" label="" date="" />
+    <Release title="Once Upon A Passion" artist="Bella Boo" label="" date="" />
     <Release
       title="Mount Liberation Unlimited"
       artist="Mount Liberation Unlimited"
       label=""
+      date=""
     />
-    <Release title="Stockholm Marathon" artist="Kornél Kovács" label="" />
+    <Release
+      title="Stockholm Marathon"
+      artist="Kornél Kovács"
+      label=""
+      date=""
+    />
     <Release
       title="Steal Chickens From Men And The Future From God"
       artist="Shakarchi & Stranéus"
       label=""
+      date=""
     />
-    <Release title="Reveries Of" artist="Art Alfie" label="" />
-    <Release title="The Bells" artist="Kornél Kovács" label="" />
-    <Release title="Total" artist="Baba Stiltz" label="" />
-    <Release title="Family Vacation" artist="Axel Boman" label="" />
-    <Release title="Kuwa Huru" artist="Usio" label="" />
+    <Release title="Reveries Of" artist="Art Alfie" label="" date="" />
+    <Release title="The Bells" artist="Kornél Kovács" label="" date="" />
+    <Release title="Total" artist="Baba Stiltz" label="" date="" />
+    <Release title="Family Vacation" artist="Axel Boman" label="" date="" />
+    <Release title="Kuwa Huru" artist="Usio" label="" date="" />
   </>
 )
 
-const Release = ({ artist, title, label }) => (
+const Release = ({ artist, title, label, date }) => (
   <div
     css={css`
       align-items: center;
