ab54217be6835f4824badd39ea779acb9a55e4c1
Luuk de Vlieger <info@luuk.site>
2019-11-17 18:43:52 +0100

    :wrench: Scaling algorithm

--- scale.js
+++ scale.js
@@ -1,7 +1,7 @@
 export default (number, type, strong = false) => `
-  ${type}: calc(${(number / (strong ? 2.25 * 1.5 : 2.25)).toFixed(1)}vw + ${(
-  number / (strong ? 1.5 * 1.5 : 1.5)
-).toFixed(1)}em);
+  ${type}: calc(${(number / (strong ? 2.5 * 1.5 : 2.5)).toFixed(3)}vw + ${(
+  number / (strong ? 1.67 * 1.5 : 1.67)
+).toFixed(3)}em);
 
   @media (min-width: 1601px) {
     ${type}: ${number}em;
