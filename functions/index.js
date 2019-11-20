b13c0e9109ad5e39c9a97929107782015bb40fae
Luuk de Vlieger <info@luuk.site>
2019-11-20 21:51:10 +0100

    :construction: API call to Discogs

--- functions/index.js
+++ functions/index.js
@@ -1,1 +1,13 @@
-exports.searchLabels = () => {}
+const got = require('got')
+
+exports.searchLabels = async (req, res) => {
+  const api = 'https://api.discogs.com/database/search'
+  const token = ''
+  const query = process && process.argv ? process.argv[1] : ''
+  const results = await got(
+    `${api}?token=${token}&per_page=10&type=label&q=${query}`,
+    { json: true }
+  )
+
+  res.status(200).send(results.body.results)
+}
