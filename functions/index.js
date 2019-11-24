63c84818d558f704f997fb7123e84d638c530cc9
Luuk de Vlieger <info@luuk.site>
2019-11-24 15:34:45 +0100

    :sparkles: New lambda option for retrieving latest label release

--- functions/index.js
+++ functions/index.js
@@ -3,10 +3,11 @@ const env = require('./env')
 
 exports.searchLabels = async (req, res) => {
   const api = 'https://api.discogs.com/database/search'
-  const results = await got(
-    `${api}?token=${env.token}&per_page=10&type=label&q=${req.query.search}`,
-    { json: true }
-  )
+  const query = req.query.search ?
+    `per_page=10&type=label&q=${req.query.search}` :
+    `per_page=1&type=master&year=${req.query.year}&format=album&label=${req.query.label}`
+
+  const results = await got(`${api}?token=${env.token}&${query}`, { json: true })
 
   res.set(
     'Access-Control-Allow-Origin',
