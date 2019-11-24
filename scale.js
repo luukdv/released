c6163c4a9a005a7a44d4734b859cc1c4a0ec5edd
Luuk de Vlieger <info@luuk.site>
2019-11-24 11:24:35 +0100

    :wrench: Slightly less scaling

--- scale.js
+++ scale.js
@@ -1,6 +1,6 @@
 export default (number, type, strong = false) => `
   ${type}: calc(${(number / (strong ? 2.5 * 1.5 : 2.5)).toFixed(1)}vw + ${(
-  number / (strong ? 1.67 * 1.5 : 1.67)
+  number / (strong ? 1.6 * 1.5 : 1.6)
 ).toFixed(1)}em);
 
   @media (min-width: 1601px) {
