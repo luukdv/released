74c7d4cade1196315a31d011453681fa54c6ae46
Luuk de Vlieger <info@luuk.site>
2019-12-25 09:51:15 +0100

    :wrench: Provide url

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,11 +1,14 @@
 const isDev = process.env.NODE_ENV === 'development'
 let endpoint
+let url
 
 if (isDev) {
   const env = require('./env')
   endpoint = env.endpoint
+  url = env.url
 } else {
   endpoint = process.env.GATSBY_API_ENDPOINT
+  url = process.env.NETLIFY_URL
 }
 
 exports.onCreateWebpackConfig = c =>
@@ -14,6 +17,7 @@ exports.onCreateWebpackConfig = c =>
       c.plugins.define({
         ANALYTICS_ID: isDev ? null : JSON.stringify(process.env.ANALYTICS_ID),
         API_ENDPOINT: JSON.stringify(endpoint),
+        NETLIFY_URL: JSON.stringify(url),
       }),
     ],
   })
