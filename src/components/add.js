c4c97c619a7bffb753b63b1b2a12fef3d89b752a
Luuk de Vlieger <info@luuk.site>
2019-12-27 14:56:58 +0100

    :art: Format

--- src/components/add.js
+++ src/components/add.js
@@ -33,7 +33,10 @@ export default React.memo(() => {
     let data
 
     try {
-      data = await get(`/.netlify/functions/searchLabels?search=${value}`, value)
+      data = await get(
+        `/.netlify/functions/searchLabels?search=${value}`,
+        value
+      )
     } catch (e) {
       setLoading(false)
       setDone(true)
