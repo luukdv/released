7c59769a684b35630be424969b450e6564445ee1
Luuk de Vlieger <info@luuk.site>
2020-01-04 23:27:45 +0100

    :fire: Remove unused param

--- scale.js
+++ scale.js
@@ -1,7 +1,7 @@
-export default (number, type, strong = false) => `
-  ${type}: calc(${(number / (strong ? 2.5 * 1.5 : 2.5)).toFixed(1)}vw + ${(
-  number / (strong ? 1.6 * 1.5 : 1.6)
-).toFixed(1)}rem);
+export default (number, type) => `
+  ${type}: calc(${(number / 2.5).toFixed(1)}vw + ${(number / 1.6).toFixed(
+  1
+)}rem);
 
   @media (min-width: 1601px) {
     ${type}: ${number}rem;
