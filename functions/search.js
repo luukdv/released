895ec3918fa77aa3a05253b89f7d94ef863a752c
Luuk de Vlieger <info@luuk.site>
2020-01-05 12:36:36 +0100

    :recycle: Strip search lambda

--- functions/search.js
+++ functions/search.js
@@ -1,59 +1,26 @@
 const axios = require('axios')
+const api = 'https://api.discogs.com/database/search'
 
 exports.handler = async e => {
-  let response
-  const api = 'https://api.discogs.com/database/search'
-  const getQuery = type => {
-    return e.queryStringParameters.query
-      ? `per_page=10&type=label&q=${e.queryStringParameters.query}`
-      : `per_page=1&type=${type}&year=${new Date().getFullYear()}&label=${
-          e.queryStringParameters.label
-        }`
-  }
-  const isMainLabel = () =>
-    results.data.results[0].label.length &&
-    results.data.results[0].label[0].toLowerCase() ===
-      e.queryStringParameters.label.toLowerCase()
+  const params = Object.entries({
+    per_page: 10,
+    q: e.queryStringParameters.query,
+    token: process.env.API_TOKEN,
+    type: 'label',
+  }).map(p => p.join('=')).join('&')
 
   try {
-    results = await axios.get(
-      `${api}?token=${process.env.API_TOKEN}&${getQuery('master')}`
-    )
-
-    if (e.queryStringParameters.query) {
-      return {
-        statusCode: 200,
-        body: JSON.stringify({
-          results: results.data.results.map(result => ({
-            id: result.id,
-            img: result.thumb,
-            link: result.uri,
-            title: result.title,
-          })),
-        }),
-      }
-    }
-
-    if (!results.data.results.length || !isMainLabel()) {
-      results = await axios.get(
-        `${api}?token=${process.env.API_TOKEN}&${getQuery('release')}`
-      )
-    }
+    const response = await axios.get(`${api}?${params}`)
 
     return {
       statusCode: 200,
       body: JSON.stringify({
-        release:
-          results.data.results.length && isMainLabel()
-            ? {
-                artist: results.data.results[0].title
-                  .split(' - ')[0]
-                  .replace(/(.+)\*$/, '$1'),
-                img: results.data.results[0].thumb,
-                link: results.data.results[0].uri,
-                title: results.data.results[0].title.split(' - ')[1],
-              }
-            : null,
+        results: response.data.results.map(result => ({
+          id: result.id,
+          img: result.thumb,
+          link: result.uri,
+          title: result.title,
+        })),
       }),
     }
   } catch (e) {
