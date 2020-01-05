4e0aa55b296553d0d589804a767051550eb78d6c
Luuk de Vlieger <info@luuk.site>
2020-01-05 13:33:08 +0100

    :recycle: Strip update lambda

--- functions/update.js
+++ functions/update.js
@@ -1,59 +1,57 @@
 const axios = require('axios')
+const api = 'https://api.discogs.com/database/search'
+const currentYear = new Date().getFullYear()
 
 exports.handler = async e => {
-  let response
-  const api = 'https://api.discogs.com/database/search'
-  const getQuery = type => {
-    return e.queryStringParameters.search
-      ? `per_page=10&type=label&q=${e.queryStringParameters.search}`
-      : `per_page=1&type=${type}&year=${new Date().getFullYear()}&label=${
-          e.queryStringParameters.name
-        }`
-  }
-  const isMainLabel = () =>
-    results.data.results[0].label.length &&
-    results.data.results[0].label[0].toLowerCase() ===
+  const getParams = (type, year) => Object.entries({
+    label: e.queryStringParameters.name,
+    per_page: 1,
+    token: process.env.API_TOKEN,
+    type,
+    year,
+  }).map(p => p.join('=')).join('&')
+
+  const isMainLabel = res =>
+    res.data.results[0].label.length &&
+    res.data.results[0].label[0].toLowerCase() ===
       e.queryStringParameters.name.toLowerCase()
 
-  try {
-    results = await axios.get(
-      `${api}?token=${process.env.API_TOKEN}&${getQuery('master')}`
-    )
+  const getLatestByYear = async year => {
+    const master = await axios.get(`${api}?${getParams('master', year)}`)
 
-    if (e.queryStringParameters.search) {
-      return {
-        statusCode: 200,
-        body: JSON.stringify({
-          results: results.data.results.map(result => ({
-            id: result.id,
-            img: result.thumb,
-            link: result.uri,
-            title: result.title,
-          })),
-        }),
-      }
+    if (master.data.results.length && isMainLabel(master)) {
+      return master
     }
 
-    if (!results.data.results.length || !isMainLabel()) {
-      results = await axios.get(
-        `${api}?token=${process.env.API_TOKEN}&${getQuery('release')}`
-      )
+    const release = await axios.get(`${api}?${getParams('release', year)}`)
+
+    if (release.data.results.length && isMainLabel(release)) {
+      return release
+    }
+  }
+
+  try {
+    let response
+
+    response = await getLatestByYear(currentYear)
+
+    if (! response) {
+      response = await getLatestByYear(currentYear - 1)
     }
 
     return {
       statusCode: 200,
       body: JSON.stringify({
-        release:
-          results.data.results.length && isMainLabel()
-            ? {
-                artist: results.data.results[0].title
-                  .split(' - ')[0]
-                  .replace(/(.+)\*$/, '$1'),
-                img: results.data.results[0].thumb,
-                link: results.data.results[0].uri,
-                title: results.data.results[0].title.split(' - ')[1],
-              }
-            : null,
+        release: response
+          ? {
+              artist: response.data.results[0].title
+                .split(' - ')[0]
+                .replace(/(.+)\*$/, '$1'),
+              img: response.data.results[0].thumb,
+              link: response.data.results[0].uri,
+              title: response.data.results[0].title.split(' - ')[1],
+            }
+          : null,
       }),
     }
   } catch (e) {
