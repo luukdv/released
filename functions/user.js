4b30b244e52e5a47b567b3756b590cbd44c8e9fc
Luuk de Vlieger <info@luuk.site>
2020-01-06 20:03:46 +0100

    :construction: Create or get user by ID

---
+++ functions/user.js
@@ -0,0 +1,35 @@
+const fauna = require('faunadb')
+const q = fauna.query
+const client = new fauna.Client({ secret: process.env.FAUNADB })
+
+exports.handler = async event => {
+  const id = event.queryStringParameters.id
+  let response
+  let data
+
+  try {
+    response = await client.query(
+      q.Get(q.Match(q.Index('by_id'), id))
+    )
+  } catch (e) {
+    try {
+      response = await client.query(
+        q.Create(q.Collection('users'), {
+          data: { id, labels: [] },
+        })
+      )
+    } catch(e) {
+      return {
+        body: JSON.stringify({
+          error: e.requestResult ? e.requestResult.responseContent.errors[0].description : e.message,
+        }),
+        statusCode: e.requestResult.statusCode ? e.requestResult.statusCode : 400,
+      }
+    }
+  }
+
+  return {
+    body: JSON.stringify({ data: response.data }),
+    statusCode: 200,
+  }
+}
