d8d731f6fb39aab524f260b90b7efc189f085337
Luuk de Vlieger <info@luuk.site>
2020-04-19 12:47:01 +0200

    :construction: Add 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Styles from '../components/styles'
+import Wrap from '../components/wrap'
+
+export default () => (
+  <>
+    <Styles />
+    <Wrap>
+      <h1>404</h1>
+    </Wrap>
+  </>
+)
