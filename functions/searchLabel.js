d40a11a8fdc84d1a6130396d58a6eb4c6ec9220a
Luuk de Vlieger <info@luuk.site>
2020-05-02 10:54:36 +0200

    :bug: Encode search query

--- functions/searchLabel.js
+++ functions/searchLabel.js
@@ -4,7 +4,7 @@ const axios = require('axios')
 exports.handler = async (event) => {
   const params = Object.entries({
     per_page: 10,
-    q: event.queryStringParameters.query,
+    q: encodeURIComponent(event.queryStringParameters.query),
     token: api.token,
     type: 'label',
   })
