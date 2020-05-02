650f83c45dbd73d3244521d3c301f8fe1393552f
Luuk de Vlieger <info@luuk.site>
2020-05-02 11:22:00 +0200

    :type: Add some context defaults

--- src/context/State.ts
+++ src/context/State.ts
@@ -1,3 +1,20 @@
-import { createContext } from 'react'
+import { createContext, Dispatch, SetStateAction } from 'react'
+import { Labels, User } from '../types'
 
-export default createContext({})
+export default createContext<{
+  done: boolean,
+  error: string,
+  labels: Labels,
+  logout: () => void,
+  setError: Dispatch<SetStateAction<string>>
+  updating: string,
+  user: User,
+}>({
+  done: false,
+  error: '',
+  labels: [],
+  logout: () => undefined,
+  setError: () => undefined,
+  updating: '',
+  user: null,
+})
